86d00084ea726cdb0076ad9fb8aa002c57615016
lucas-tachet-dev <lucastachet.dev@gmail.com>
2026-06-10 15:54:38 +0200

    adding script projets display projects from API

---
+++ FrontEnd/assets/projets.js
@@ -0,0 +1,41 @@
+
+// Récup éventuelle des projets dans le localStorage
+let works = localStorage.getItem("works");
+
+// Récup dans l'API si rien dans le localStorage
+if (works === null) {
+    // Récup de l'API
+    const response = await fetch("http://localhost:5678/api/works");
+    works = await response.json();
+    
+    // Transformation des projets en JSON
+    const valueWorks = JSON.stringify(works);
+
+    // Stockage dans le localStorage
+    localStorage.setItem("works", valueWorks);
+} else {
+    works = JSON.parse(works)
+};
+
+// Génération des projets sur la page
+function displayWorks(works) {
+    for(let i = 0; i < works.length; i++){
+        const work = works[i];
+
+        // Récup de l'élement DOM pour la figure
+        const gallery = document.querySelector(".gallery");
+
+        // Création de la figure
+        const workElement = document.createElement("figure")
+        workElement.dataset.id = work.id
+        // Création du code HTML dans l'élément
+        workElement.innerHTML = `
+        <img src="${work.imageUrl}" alt="${work.title}">
+        <figcaption>${work.title}</figcaption>`;
+
+        //Insertion de l'élément dans le DOM
+        gallery.appendChild(workElement);
+    }
+};
+
+displayWorks(works);
