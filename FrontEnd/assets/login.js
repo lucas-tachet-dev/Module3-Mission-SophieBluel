128fdc1beaa4cc67c730a5e90dd1a7113537a2c8
lucas-tachet-dev <lucastachet.dev@gmail.com>
2026-06-12 16:51:23 +0200

    added Login functionnality

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,43 @@
+
+
+// Submit du login
+const login = document.querySelector("#login");
+
+login.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    //Récup des elem d'email et password
+    const emailInput = document.querySelector("#email");
+    const passwordInput = document.querySelector("#password");
+
+    //Récup de leurs valeurs
+    const emailValue = emailInput.value;
+    const passwordValue = passwordInput.value;
+
+    //Stockage dans un objet
+    const loginData = {
+        email: emailValue,
+        password: passwordValue
+    }
+
+    //envoie d'une requête POST à l'API
+    const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(loginData)
+    })
+
+    //Test du login
+    if(response.ok){
+        const data = await response.json();
+
+        //Stockage du token dans la sessionStorage
+        sessionStorage.setItem("token", data.token);
+
+        //Redirection vers l'index
+        window.location.href = "index.html";
+    } else {
+        alert("Email/Mot de passe incorrect")
+    }
+})
+
