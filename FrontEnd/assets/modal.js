2c77b7ad70bb29633dfa93219b469477827714b9
lucas-tachet-dev <lucastachet.dev@gmail.com>
2026-06-21 14:01:28 +0200

    fixing details of modal

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -4,7 +4,7 @@ let categories = localStorage.getItem("categories");
 // Récup dans l'API si rien dans le localStorage
 if (categories === null) {
     // Récup de l'API
-    const response = await fetch("http://localhost:5678/api/works");
+    const response = await fetch("http://localhost:5678/api/categories");
     categories = await response.json();
     
     // Transformation des projets en JSON
@@ -16,15 +16,20 @@ if (categories === null) {
     categories = JSON.parse(categories)
 };
 
+// Pages de la modale
 const modalElement = document.createElement("div");
 
 const photoGallery = `
-<div>
+<div class="photo-gallery">
     <h2>Galerie Photo</h2>
     <div class="modal-grid">
     </div>
-    <button>Ajouter une photo</button>
+    <button class="btn-add-photo">Ajouter une photo</button>
 </div>`;
+
+const generateCategoryOptions = () => {
+    return categories.map(cat => `<option value="${cat.id}">${cat.name}</option>`).join('');
+};
 
 const addPhoto = `
 <div class="modal-content">
@@ -36,23 +41,23 @@ const addPhoto = `
                 <span>+ Ajouter une photo</span>
                 <p>jpg, png : 4mo max</p>
             </label>
-            <input type="file" id="photo-upload" accept="image/jpg, image/png" />
+            <input type="file" id="photo-upload" accept="image/jpeg, image/png" />
         </div>
         <div class="photo-info">
             <label for="photo-title">Titre</label>
-            <input type="text" name="photo-title" id="photo-title" />
+            <input type="text" name="photo-title" id="photo-title" required />
+            
             <label for="category-select">Catégorie</label>
-            <select name="category" id="category-select">
+            <select name="category" id="category-select" required>
                 <option value=""></option>
-                <option value="objets" id="${categories[0].id}">${categories[0].name}</option>
-                <option value="appartement" id="${categories[1].id}">${categories[1].name}</option>
-                <option value="hotel" id="${categories[2].id}">${categories[2].name}</option>
+                ${generateCategoryOptions()} 
             </select>
         </div>
+        <button type="submit" class="btn-validate">Valider</button>
     </form>
-    <button>Valider</button>
 </div>`;
 
+// Fonctions de la modale
 export function displayModal(works){
     const portfolioElement = document.getElementById("portfolio");
     
@@ -60,15 +65,15 @@ export function displayModal(works){
         <aside id="edit-modal" class="modal" aria-hidden="true" role="dialog" aria-labelledby="modal-title">
             <div class="modal-wrapper">
                 <div class="modal-btns">
-					<img class="modal-back" src="assets/icons/arrow-left-solid-full.svg" alt="Retour en arrière" />
-                    <img class="modal-close" src="assets/icons/xmark-solid-full.svg" alt="Fermeture de la fenêtre d'edition" />
+                    <img class="modal-back" src="assets/icons/arrow-left-solid-full.svg" alt="Retour en arrière" />
+                    <img class="modal-close" src="assets/icons/xmark-solid-full.svg" alt="Fermeture de la fenêtre d'édition" />
                 </div>
-                ${photoGallery}
-            </div>
+                <div class="modal-body"></div> </div>
         </aside>`;
 
     portfolioElement.insertAdjacentElement("beforebegin", modalElement);
     
+    // Écouteurs de fermeture
     const closeIcon = document.querySelector(".modal-close");
     closeIcon.addEventListener("click", closeModal);
 
@@ -77,26 +82,56 @@ export function displayModal(works){
         if (event.target === event.currentTarget) {
             closeModal();
         }
+    });;
+
+    
+    const backButton = document.querySelector(".modal-back");
+    backButton.addEventListener("click", () => {
+        showGallery(works)
     });
-
-    for(let i = 0; i < works.length; i++){
-        const work = works[i];
-
-        // Récup de l'élement DOM pour la figure
-        const modalGrid = document.querySelector(".modal-grid");
-
-        // Création de la figure
-        const modalElement = document.createElement("figure")
-        modalElement.dataset.id = work.id
-        // Création du code HTML dans l'élément
-        modalElement.innerHTML = `
-        <img class="modal-img" src="${work.imageUrl}" alt="${work.title}" />
-        <img class="delete-icon" id="delete${work.id}" src="assets/icons/trash-can-solid-full.svg" alt="Supprimer ${work.title}"/>`;
-
-        //Insertion de l'élément dans le DOM
-        modalGrid.appendChild(modalElement);
-    }
+    
+    showGallery(works);
 };
+
+function showGallery(works){
+    modalElement.querySelector(".modal-back").classList.remove("visible");
+    
+    const modalBody = modalElement.querySelector(".modal-body");
+    modalBody.innerHTML = photoGallery;
+    
+    // Écouteur pour la partie d'ajout de photo
+    const addPhotoButton = document.querySelector(".btn-add-photo");
+    addPhotoButton.addEventListener("click", switchAddPhoto);
+
+    // Récup de l'élement DOM pour la figure
+    const modalGrid = document.querySelector(".modal-grid");
+    
+    // Replissage de la grille
+    works.forEach(work => {
+        // Création de la figure
+        const figure = document.createElement("figure");
+        
+        // Définir son id
+        figure.dataset.id = work.id;
+
+        // Création de son HTML
+        figure.innerHTML = `
+            <img class="modal-img" src="${work.imageUrl}" alt="${work.title}" />
+            <img class="delete-icon" id="delete-${work.id}" src="assets/icons/trash-can-solid-full.svg" alt="Supprimer ${work.title}"/>
+        `;
+        modalGrid.appendChild(figure);
+    })
+}
+
+function switchAddPhoto() {
+    modalElement.querySelector(".modal-back").classList.add("visible");
+    
+    const modalBody = modalElement.querySelector(".modal-body");
+    
+    // Remplacement de la galerie par l'ajout de photo
+    modalBody.innerHTML = addPhoto;
+    
+}
 
 function closeModal(){
     modalElement.remove();
